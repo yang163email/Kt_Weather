80d68ed31b97634d4066605ad1a0c05fbf7d083d
yanganan <13640662396@163.com>
2017-12-13 17:44:30 +0800

    application单例实现，使用代理

---
+++ app/src/main/java/com/yan/weather/app/WeatherApp.kt
@@ -0,0 +1,21 @@
+package com.yan.weather.app
+
+import android.app.Application
+import com.yan.weather.utils.DelegatesExt
+
+/**
+ *  @author      : 楠GG
+ *  @date        : 2017/12/13 17:17
+ *  @description : TODO
+ */
+class WeatherApp : Application() {
+
+    companion object {
+        private var instance: WeatherApp by DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
---
+++ app/src/main/java/com/yan/weather/utils/DelegatesExt.kt
@@ -0,0 +1,26 @@
+package com.yan.weather.utils
+
+import kotlin.reflect.KProperty
+
+/**
+ *  @author      : 楠GG
+ *  @date        : 2017/12/13 17:34
+ *  @description : TODO
+ */
+class NotNullSingleValueVar<T> {
+
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value ?: throw IllegalStateException("${property.name} not initialized")
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (value == null) value
+        else throw IllegalStateException("${property.name} already initialized")
+    }
+}
+
+object DelegatesExt {
+    fun <T> notNullSingleValue() = NotNullSingleValueVar<T>()
+}
